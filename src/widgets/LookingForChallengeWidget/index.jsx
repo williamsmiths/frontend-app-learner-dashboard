afe356b9f08049c29cf11d2b24cf311f917afef8
Thien Mai <63228267+thienmdp@users.noreply.github.com>
2025-09-11 16:31:02 +0700

    fix: img

--- src/widgets/LookingForChallengeWidget/index.jsx
+++ src/widgets/LookingForChallengeWidget/index.jsx
@@ -1,35 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { useIntl } from '@edx/frontend-platform/i18n';
-import { Card, Hyperlink, Icon } from '@openedx/paragon';
-import { ArrowForward } from '@openedx/paragon/icons';
+import { useIntl } from "@edx/frontend-platform/i18n";
+import { Card, Hyperlink, Icon } from "@openedx/paragon";
+import { ArrowForward } from "@openedx/paragon/icons";
 
-import { reduxHooks } from 'hooks';
-import moreCoursesSVG from 'assets/logo-dtu.png';
-import { baseAppUrl } from 'data/services/lms/urls';
+import { reduxHooks } from "hooks";
+import moreCoursesSVG from "assets/logo-dtu.png";
+import { baseAppUrl } from "data/services/lms/urls";
 
-import { findCoursesWidgetClicked } from './track';
-import messages from './messages';
-import './index.scss';
+import { findCoursesWidgetClicked } from "./track";
+import messages from "./messages";
+import "./index.scss";
 
-export const arrowIcon = (<Icon className="mx-1" src={ArrowForward} />);
+export const arrowIcon = <Icon className="mx-1" src={ArrowForward} />;
 
 export const LookingForChallengeWidget = () => {
   const { formatMessage } = useIntl();
   const { courseSearchUrl } = reduxHooks.usePlatformSettingsData();
-  const hyperlinkDestination = baseAppUrl(courseSearchUrl) || '';
+  const hyperlinkDestination = baseAppUrl(courseSearchUrl) || "";
 
   return (
     <Card orientation="horizontal" id="looking-for-challenge-widget">
-      <Card.ImageCap
+      {/* <Card.ImageCap
         src={moreCoursesSVG}
         srcAlt="course side widget"
         className="m-3 border-start border-4 border-warning"
-      />
+      /> */}
+      <div className="m-3 border-start border-4 border-warning">
+        <img src={moreCoursesSVG} />
+      </div>
       <Card.Body className="m-auto pr-2">
-        <h4>
-          {formatMessage(messages.lookingForChallengePrompt)}
-        </h4>
+        <h4>{formatMessage(messages.lookingForChallengePrompt)}</h4>
         <h5>
           <Hyperlink
             variant="link"
