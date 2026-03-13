124d91396c9e08fb829c3b23d78c95035a5143f1
letientai <letai97md@gmail.com>
2026-03-13 16:01:11 +0700

    fix: update text to Vietnamese translation for recommended courses

--- src/plugin-slots/WidgetSidebarSlot/index.jsx
+++ src/plugin-slots/WidgetSidebarSlot/index.jsx
@@ -10,7 +10,7 @@ export const WidgetSidebarSlot = () => (
     idAliases={['widget_sidebar_slot']}
   >
     <div className="mb-2 text-dark fs-5" style={{ fontWeight: '700' }}>
-          Recommended Courses for You
+          Khóa học được đề xuất cho bạn
     </div>
 
     <LookingForChallengeWidget />
